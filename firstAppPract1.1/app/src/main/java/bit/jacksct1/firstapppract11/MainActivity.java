2807f91123f0df47d22a24e5fcb76f5a386114a5
iiSABRE <jacksct1@D207-61588.op.ac.nz>
2016-02-19 14:54:37 +1300

    First Code 1.2a complete

--- firstAppPract1.1/app/src/main/java/bit/jacksct1/firstapppract11/MainActivity.java
+++ firstAppPract1.1/app/src/main/java/bit/jacksct1/firstapppract11/MainActivity.java
@@ -2,6 +2,9 @@ package bit.jacksct1.firstapppract11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView headerString = (TextView) findViewById(R.id.txtviewHeader);
+        String dogBreed = "";
+        Random rBreed = new Random();
+        int pickBreed = rBreed.nextInt(4);
+
+        switch(pickBreed)
+        {
+            case 0:
+                headerString.setText("Poodle");
+                break;
+            case 1:
+                headerString.setText("Labrador");
+                break;
+            case 2:
+                headerString.setText("Shar Pei");
+                break;
+            case 3:
+                headerString.setText("Newfoundland");
+                break;
+
+        }
+
+
     }
+
+
 }

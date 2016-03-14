9aca09fcc36c73c189c620e88e8e94275304b311
iiSABRE <JACKSCT1@student.op.ac.nz>
2016-03-14 13:59:37 +1300

    List Fragment Class Written
    Completed Class and Set Listen for button

--- IntroToFragments/app/src/main/java/bit/jacksct1/introtofragments/MainActivity.java
+++ IntroToFragments/app/src/main/java/bit/jacksct1/introtofragments/MainActivity.java
@@ -17,8 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnImageFragment = (Button) findViewById(R.id.btnImageView);
-        ViewImageFragment handler = new ViewImageFragment();
-        btnImageFragment.setOnClickListener(handler);
+        ViewImageFragment imageHandler = new ViewImageFragment();
+        btnImageFragment.setOnClickListener(imageHandler);
+
+        Button btnListFragment = (Button) findViewById(R.id.btnListView);
+        ViewListFragment listHandler = new ViewListFragment();
+        btnListFragment.setOnClickListener(listHandler);
     }
 
     public class ViewImageFragment implements View.OnClickListener
@@ -42,11 +46,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v)
         {
-            Fragment imageFragment = new ShowListFragment();
+            Fragment listFragment = new ShowListFragment();
             FragmentManager fm = getFragmentManager();
 
             FragmentTransaction ft = fm.beginTransaction();
-            ft.replace(R.id.fragment_container_image, imageFragment);
+            ft.replace(R.id.fragment_container_image,listFragment);
             ft.commit();
         }
     }
--- IntroToFragments/app/src/main/java/bit/jacksct1/introtofragments/ShowListFragment.java
+++ IntroToFragments/app/src/main/java/bit/jacksct1/introtofragments/ShowListFragment.java
@@ -1,11 +1,14 @@
 package bit.jacksct1.introtofragments;
 
 import android.app.Fragment;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by jacksct1 on 14/03/2016.
@@ -18,15 +21,13 @@ public class ShowListFragment extends Fragment
     {
         View fragmentView = inflater.inflate(R.layout.show_list_fragment, container, false);
 
+        ListView lvCapitals = (ListView) fragmentView.findViewById(R.id.lvCapitals);
+        Resources resource = getResources();
+        String[] capitalArray = resource.getStringArray(R.array.CapitalsArray);
 
+        ArrayAdapter<String> capitalAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, capitalArray);
 
-
-
-
-
-
-
-
+        lvCapitals.setAdapter(capitalAdapter);
 
         return fragmentView;
     }

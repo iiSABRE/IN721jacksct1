9622a641c597dd5b8c11d4c69387ddd8a2c52cab
iiSABRE <cameronthomasjackson@gmail.com>
2016-04-07 18:15:34 +1200

    Database fully populated

--- WesterosHousesDatabase/app/src/main/java/bit/jacksct1/westeroshousesdatabase/SearchScreen.java
+++ WesterosHousesDatabase/app/src/main/java/bit/jacksct1/westeroshousesdatabase/SearchScreen.java
@@ -61,7 +61,54 @@ public class SearchScreen extends AppCompatActivity
         westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Bran', 'Stark', 'Stark')");
         westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Ayra', 'Stark', 'Stark')");
         westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Catelyn', 'Stark', 'Stark')");
-        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Eddard', 'Stark', 'Stark')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Jon', 'Arryn', 'Arryn')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Lysa', 'Arryn', 'Arryn')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Robin', 'Arryn', 'Arryn')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Robert', 'Baratheon', 'Baratheon')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Stannis', 'Baratheon', 'Baratheon')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Renly', 'Baratheon', 'Baratheon')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Joffrey', 'Baratheon', 'Baratheon')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Myrcella', 'Baratheon', 'Baratheon')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Tommen', 'Baratheon', 'Baratheon')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Roose', 'Bolton', 'Bolton')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Walda', 'Bolton', 'Bolton')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Ramsay', 'Bolton', 'Bolton')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Walder', 'Frey', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Joyeuse', 'Frey', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Stenron', 'Frey', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Lothar', 'Frey', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Roslin', 'Frey', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Walder', 'Rivers', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Ryger', 'Rivers', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Balon', 'Greyjoy', 'Greyjoy')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Euron', 'Rivers', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Rodrik', 'Rivers', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Yara', 'Rivers', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Theon', 'Rivers', 'Frey')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Tywin', 'Lannister', 'Lannister')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Cersei', 'Lannister', 'Lannister')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Jamie', 'Lannister', 'Lannister')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Tyrion', 'Lannister', 'Lannister')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Lancel', 'Lannister', 'Lannister')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Doran', 'Martell', 'Martell')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Elia', 'Martell', 'Martell')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Oberyn', 'Martell', 'Martell')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Trystane', 'Martell', 'Martell')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Aemon', 'Targaryen', 'Targaryen')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Aegon', 'Targaryen', 'Targaryen')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Aerys', 'Targaryen', 'Targaryen')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Rhaegar', 'Targaryen', 'Targaryen')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Viserys', 'Targaryen', 'Targaryen')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Daenerys', 'Targaryen', 'Targaryen')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Olenna', 'Tyrell', 'Tyrell')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Mace', 'Tyrell', 'Tyrell')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Margaery', 'Tyrell', 'Tyrell')");
+        westerosPeopleAndHousesDB.execSQL("INSERT INTO tblPerson VALUES(null, 'Loras', 'Tyrell', 'Tyrell')");
+
+
+
+
+
 
 
 

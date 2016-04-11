0679ee108a9cee8d46d97eb4d2ef5b114cb2769a
iiSABRE <cameronthomasjackson@gmail.com>
2016-04-12 10:21:13 +1200

    Initial Create

---
+++ Week 8/FormattedFileIO/app/src/main/java/bit/jacksct1/formattedfileio/FillListScreen.java
@@ -0,0 +1,19 @@
+package bit.jacksct1.formattedfileio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FillListScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_list_screen);
+
+    }
+
+    public void getDataList()
+    {
+
+    }
+}

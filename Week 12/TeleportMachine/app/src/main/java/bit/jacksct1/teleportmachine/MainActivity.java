b6e8e0d97496a01d7c9a13f982b79eb9546836c6
iiSABRE <cameronthomasjackson@gmail.com>
2016-05-10 13:38:37 +1200

    Async Working with hard coded latLong

--- Week 12/TeleportMachine/app/src/main/java/bit/jacksct1/teleportmachine/MainActivity.java
+++ Week 12/TeleportMachine/app/src/main/java/bit/jacksct1/teleportmachine/MainActivity.java
@@ -1,18 +1,30 @@
 package bit.jacksct1.teleportmachine;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Random generator = new Random();
-    int max = 90;
-    int min = -90;
+    String latitude;
+    String longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button teleport = (Button) findViewById(R.id.btnTeleport);
         teleport.setOnClickListener(new teleportClick());
+
+
 
     }
 
@@ -34,6 +48,86 @@ public class MainActivity extends AppCompatActivity {
 
             TextView tvLat = (TextView) findViewById(R.id.tvLatValue);
             tvLat.setText(new Manager().getLat());
+
+            WebService APIThread = new WebService();
+            APIThread.execute();
         }
+    }
+
+    class WebService extends AsyncTask<Void,Void,String>
+    {
+
+        @Override
+        protected String doInBackground(Void... params)
+        {
+            String JSONString = null;
+
+            try
+            {
+                String urlString = "http://www.geoplugin.net/extras/location.gp?lat=-45.8787605&long=170.5027976&format=json";
+
+                URL URLObject = new URL(urlString);
+                HttpURLConnection connection = (HttpURLConnection) URLObject.openConnection();
+                connection.connect();
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode != 200)
+                {
+                    Toast.makeText(MainActivity.this, "Failed to retrieve data from web", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    InputStream inputStream = connection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    String responseString;
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while((responseString = bufferedReader.readLine()) != null)
+                    {
+                        stringBuilder = stringBuilder.append(responseString);
+                    }
+                    JSONString = stringBuilder.toString();
+                }
+
+            } catch (MalformedURLException e) {
+                Toast.makeText(MainActivity.this, "Failed to get JSONString", Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Toast.makeText(MainActivity.this, "Failed to get JSONString", Toast.LENGTH_LONG).show();
+            }
+
+            return JSONString;
+        }
+
+        @Override
+        protected void onPostExecute(String fetchedString)
+        {
+
+            try {
+                if (fetchedString == "[[]]")
+                {
+                    Toast.makeText(MainActivity.this, "No nearest city", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    JSONObject nearestCity = new JSONObject(fetchedString);
+                    String closestcity = nearestCity.getString("geoplugin_place");
+                    String country = nearestCity.getString("geoplugin_countryCode");
+                    TextView nearest = (TextView) findViewById(R.id.tvNearest);
+                    nearest.setText("Closest City: " + closestcity +", " + country);
+
+                }
+
+
+
+            }
+            catch (JSONException e)
+            {
+                Toast.makeText(MainActivity.this,"Couldnt get location information", Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+
     }
 }
